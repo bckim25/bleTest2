87b050fc7cf393a922964c78254b2362258db671
peppi <peppi@knou.ac.kr>
2021-10-06 08:55:22 +0900

    Add project files.

---
+++ bleTest2/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace bleTest2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnScan = new System.Windows.Forms.Button();
+            this.btnRst = new System.Windows.Forms.Button();
+            this.listRec = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // btnScan
+            // 
+            this.btnScan.Location = new System.Drawing.Point(208, 298);
+            this.btnScan.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnScan.Name = "btnScan";
+            this.btnScan.Size = new System.Drawing.Size(116, 34);
+            this.btnScan.TabIndex = 0;
+            this.btnScan.Text = "scan";
+            this.btnScan.UseVisualStyleBackColor = true;
+            this.btnScan.Click += new System.EventHandler(this.btnScan_Click);
+            // 
+            // btnRst
+            // 
+            this.btnRst.Location = new System.Drawing.Point(366, 298);
+            this.btnRst.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnRst.Name = "btnRst";
+            this.btnRst.Size = new System.Drawing.Size(116, 34);
+            this.btnRst.TabIndex = 0;
+            this.btnRst.Text = "result";
+            this.btnRst.UseVisualStyleBackColor = true;
+            this.btnRst.Click += new System.EventHandler(this.btnRst_Click);
+            // 
+            // listRec
+            // 
+            this.listRec.FormattingEnabled = true;
+            this.listRec.ItemHeight = 12;
+            this.listRec.Location = new System.Drawing.Point(26, 19);
+            this.listRec.Name = "listRec";
+            this.listRec.Size = new System.Drawing.Size(655, 244);
+            this.listRec.TabIndex = 1;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(700, 360);
+            this.Controls.Add(this.listRec);
+            this.Controls.Add(this.btnRst);
+            this.Controls.Add(this.btnScan);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnScan;
+        private System.Windows.Forms.ListBox tb;
+        private System.Windows.Forms.Button btnRst;
+        private System.Windows.Forms.ListBox listRec;
+    }
+}
+
---
+++ bleTest2/Form1.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Windows.Devices.Bluetooth;
+using Windows.Devices.Bluetooth.GenericAttributeProfile;
+using Windows.Devices.Enumeration;
+using System.Windows.Threading;
+
+namespace bleTest2
+{
+    public partial class Form1 : Form
+    {
+        static DeviceInformation device = null;
+        static public List<string> items = new List<string>();
+
+        public delegate int updateDelegate();
+
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btnScan_Click(object sender, EventArgs e)
+        {
+            Main();
+        }
+
+        static async Task Main()
+        {
+            // Query for extra properties you want returned
+            string[] requestedProperties = { "System.Devices.Aep.DeviceAddress", "System.Devices.Aep.IsConnected" };
+
+            DeviceWatcher deviceWatcher =
+                        DeviceInformation.CreateWatcher(
+                                BluetoothLEDevice.GetDeviceSelectorFromPairingState(false),
+                                requestedProperties,
+                                DeviceInformationKind.AssociationEndpoint);
+
+            // Register event handlers before starting the watcher.
+            // Added, Updated and Removed are required to get all nearby devices
+            deviceWatcher.Added += DeviceWatcher_Added;
+            deviceWatcher.Updated += DeviceWatcher_Updated;
+            deviceWatcher.Removed += DeviceWatcher_Removed;
+
+            // EnumerationCompleted and Stopped are optional to implement.
+            deviceWatcher.EnumerationCompleted += DeviceWatcher_EnumerationCompleted;
+            deviceWatcher.Stopped += DeviceWatcher_Stopped;
+
+            // Start the watcher.
+            deviceWatcher.Start();
+            while (true)
+            {
+                if (device == null)
+                {
+                    Thread.Sleep(500);
+                }
+                else
+                {
+                    Console.WriteLine("PRess Any to pair with Wahoo Ticker");
+                    Console.ReadKey();
+                    BluetoothLEDevice bluetoothLeDevice = await BluetoothLEDevice.FromIdAsync(device.Id);
+                    Console.WriteLine("Attempting to pair with device");
+                    GattDeviceServicesResult result = await bluetoothLeDevice.GetGattServicesAsync();
+
+                    if (result.Status == GattCommunicationStatus.Success)
+                    {
+                        Console.WriteLine("Pairing succeeded");
+                        var services = result.Services;
+                        foreach (var service in services)
+                        {
+                            Console.WriteLine(service.Uuid);
+                        }
+                    }
+                    Console.WriteLine("Press Any Key to Exit application");
+                    Console.ReadKey();
+                    break;
+                }
+
+            }
+
+        }
+
+        private static void DeviceWatcher_Stopped(DeviceWatcher sender, object args)
+        {
+            /*throw new NotImplementedException();*/
+        }
+
+        private static void DeviceWatcher_EnumerationCompleted(DeviceWatcher sender, object args)
+        {
+            /*throw new NotImplementedException();*/
+        }
+
+        private static void DeviceWatcher_Removed(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            /*throw new NotImplementedException();*/
+        }
+
+        private static void DeviceWatcher_Updated(DeviceWatcher sender, DeviceInformationUpdate args)
+        {
+            /*throw new NotImplementedException();*/
+        }
+
+        private static void DeviceWatcher_Added(DeviceWatcher sender, DeviceInformation args)
+        {
+
+
+            if (args.Name == "Nugawinder")
+            {
+                Console.WriteLine(args.Name);
+                device = args;
+                Console.WriteLine("id : " + args.Id);
+                items.Add(args.Name);
+
+            }
+
+            //throw new NotImplementedException();
+        }
+
+        private void updateDeviceList()
+        {
+            /*listRec.DataSource = items;*/
+        }
+
+        private void btnRst_Click(object sender, EventArgs e)
+        {
+            listRec.DataSource = items;
+            for(int i = 0; i < items.Count; i++)
+            {
+                /*listRec.Items = items[i];*/
+            }
+
+        }
+
+
+    }
+}
